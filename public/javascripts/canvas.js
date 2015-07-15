29f8d182c7ee8cb32c61abf556a4aec47e101e55
Steven Abrams <steven.abrams86@gmail.com>
2015-07-15 15:42:24 -0600

    complete Project

--- public/javascripts/canvas.js
+++ public/javascripts/canvas.js
@@ -25,7 +25,7 @@ Square.prototype.constructor = Square;
 Square.prototype.draw = function () {
   ctx.fillStyle = this.color;
   ctx.fillRect(this.xcord, this.ycord, this.width, this.width);
-  allSquares.push([this.xcord, this.ycord, this.width, this.width]);
+  allSquares.push([this.xcord, this.ycord, this.width, this.width, this.color]);
 }
 
 //************
@@ -44,29 +44,29 @@ Circle.prototype.draw = function () {
   ctx.arc(this.xcord, this.ycord, this.width, 0, 2 * Math.PI, false);
   ctx.closePath();
   ctx.fill();
-  allCircles.push([this.xcord, this.ycord, this.width, this.width]);
+  allCircles.push([this.xcord, this.ycord, this.width, this.width, this.color]);
 }
 
-var paintShapes = function (color) {
+var paintSquares = function (color) {
   allSquares.forEach(function (square) {
-    var s = new Square(square[0],square[1],square[2],color);
+    var s = new Square(square[0],square[1],square[2],color || square[4]);
+    square[3] = color;
     s.draw();
   })
+}
+var paintCircles = function (color) {
   allCircles.forEach(function (circle) {
-    var c = new Circle(circle[0],circle[1],circle[2],color);
+    var c = new Circle(circle[0],circle[1],circle[2],color || circle[4]);
+    circle[3] = color;
     c.draw();
   })
 }
+var paintShapes = function (color) {
+  paintSquares(color);
+  paintCircles(color);
+}
 var paintRandom = function () {
   var colorArr = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'];
-  allSquares.forEach(function (square) {
-    var randomColor = '#';
-    for (var i = 0; i < 6; i++){
-      randomColor += colorArr[Math.floor(Math.random() * colorArr.length)];
-    }
-    var s = new Square(square[0],square[1],square[2],randomColor);
-    s.draw();
-  })
   allCircles.forEach(function (circle) {
     var randomColor = '#';
     for (var i = 0; i < 6; i++){
@@ -75,11 +75,22 @@ var paintRandom = function () {
     var c = new Circle(circle[0],circle[1],circle[2],randomColor);
     c.draw();
   })
+  allSquares.forEach(function (square) {
+    var randomColor = '#';
+    for (var i = 0; i < 6; i++){
+      randomColor += colorArr[Math.floor(Math.random() * colorArr.length)];
+    }
+    var s = new Square(square[0],square[1],square[2],randomColor);
+    s.draw();
+  })
+
 }
 var clearSquares = function() {
   allSquares.forEach(function (square) {
     ctx.clearRect(square[0],square[1],square[2],square[3]);
   });
+  allSquares = [];
+  paintCircles();
 }
 var fillRandom = function () {
   var colorArr = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'];
@@ -88,16 +99,10 @@ var fillRandom = function () {
     for (var j = 0; j < 6; j++){
       randomColor += colorArr[Math.floor(Math.random() * colorArr.length)];
     }
-    console.log(i, randomColor);
     var x = Math.floor(Math.random() * 500);
     var y = Math.floor(Math.random() * 500);
-    console.log(i, x, y);
     var randomSize = Math.floor(Math.random() * 100) + 1;
-    console.log(i, randomSize);
-
     var randomShape = Math.floor(Math.random()*2);
-    console.log(i, randomShape);
-
     if (randomShape === 0){
       var c = new Circle(x,y,randomSize,randomColor);
       c.draw();
